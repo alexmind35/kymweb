60ff2e586929635316b1434fc10b75327d26c544
Александр Михайлов <38732070+alexmind35@users.noreply.github.com>
2020-01-14 20:34:22 +0300

    Начало! Без форм регистрации. Для примеров из книги

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -4,11 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HeroView.as_view(), name='home'),
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
-    path('register/', views.register_user, name='register'),
-    path('edit_profile/', views.edit_profile, name='edit_profile'),
-    path('change_password', views.change_password, name='change_password'),
-    path('dashboard/', views.dashboard_view, name='dashboard'),
+
 
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,12 +1,8 @@
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import PasswordChangeForm
-from django.shortcuts import render, redirect
+
+from django.shortcuts import render
 
 from django.views.generic.base import View
 
-from .forms import EditProfileForm, SignUpForm
 from .models import Hero
 from .models import Services
 from .models import Why
@@ -27,79 +23,3 @@ class HeroView(View):
                           "work_list": work_section,
                       })
 
-
-def login_user(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('dashboard')
-
-        else:
-            messages.success(request, ('Ошибка! Неправильный логин или пароль'))
-            return redirect('login')
-    else:
-        return render(request, 'pages/login_page.html', {})
-
-
-
-
-
-def logout_user(request):
-    logout(request)
-    return redirect('home')
-
-
-def register_user(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            messages.success(request, ('You Have Registered...'))
-            return redirect('home')
-    else:
-        form = SignUpForm()
-
-    context = {'form': form}
-    return render(request, 'pages/register_page.html', context)
-
-
-def edit_profile(request):
-    if request.method == 'POST':
-        form = EditProfileForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            messages.success(request, ('You Have Edited Your Profile...'))
-            return redirect('home')
-    else:
-        form = EditProfileForm(instance=request.user)
-
-    context = {'form': form}
-    return render(request, 'pages/edit_profile.html', context)
-
-
-def change_password(request):
-    if request.method == 'POST':
-        form = PasswordChangeForm(data=request.POST, user=request.user)
-        if form.is_valid():
-            form.save()
-            update_session_auth_hash(request, form.user)
-            messages.success(request, ('You Have Edited Your Password...'))
-            return redirect('home')
-    else:
-        form = PasswordChangeForm(user=request.user)
-
-    context = {'form': form}
-    return render(request, 'pages/change_password.html', context)
-
-
-@login_required
-def dashboard_view(request):
-    messages.success(request, ('Авторизуйтесь!Введите логин и пароль'))
-    return render(request, "pages/admin_page.html")
